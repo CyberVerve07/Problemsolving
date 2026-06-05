b76336a16204244b45ba3c9551179095a820bc2b
CyberVerve07 <adityamahajan7287@gmail.com>
2026-06-05 12:06:33 +0530

    feat: add new practice exercises and problem solutions for basic Java operations

---
+++ src/Practice/code1.java
@@ -0,0 +1,17 @@
+package Practice;
+
+public class code1 {
+    public static void main(String[] args) {
+        //Bitwise operatoes
+        //| Operator | Name        |    |
+        //| -------- | ----------- | -- |
+        //| `&`      | AND         |    |
+        //| `        | `           | OR |
+        //| `^`      | XOR         |    |
+        //| `~`      | NOT         |    |
+        //| `<<`     | Left Shift  |    |
+        //| `>>`     | Right Shift |    |
+        int a=4&4;
+        System.out.println(a);
+    }
+}
--- src/Problems/Problem4.java
+++ src/Problems/Problem4.java
@@ -2,13 +2,12 @@ package Problems;
 
 import java.util.Scanner;
 
-public class Problem4 {
+public class Problem4{
     public static void main(String[] args) {
         Scanner input =new Scanner(System.in);
         System.out.println("Enter celciuss");
         int celcius= input.nextInt();
         int F;
         F = (celcius * 9/5) + 32;
-        System.out.println("The Temperature is "+F);
-    }
+        System.out.println("The Temperature is "+F); }
 }
