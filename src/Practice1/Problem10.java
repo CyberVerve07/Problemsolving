ba6a5ab868888613db30d0dc76574b050a0e7cdf
CyberVerve07 <adityamahajan7287@gmail.com>
2026-06-15 21:00:37 +0530

    feat: add new practice problems and e-commerce order simulation scripts

---
+++ src/Practice1/Problem10.java
@@ -0,0 +1,21 @@
+package Practice1;
+
+public class Problem10 {
+
+    static void tabel(){
+
+        for(int i=1;i<=10;i++){
+            System.out.println(i*100);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        tabel();
+        tabel();
+        tabel();
+        tabel();
+        tabel();
+        tabel();
+    }
+}
