04d038e347929491698889af031949ec576d505f
CyberVerve07 <adityamahajan7287@gmail.com>
2026-07-02 11:42:37 +0530

    feat: add compiled class files for July 2 problem set

---
+++ src/July2/code10.java
@@ -0,0 +1,32 @@
+package July2;
+
+public class code10 {
+    //Implement a Java program to simulate a simple user profile.
+    //
+    //Requirements:
+    //
+    //Store first name and father's name using String literals.
+    //Append a surname using concat().
+    //Create another user using the new keyword.
+    //Compare both users using:
+    //==
+    //equalsIgnoreCase()
+    //
+    //Print all results.
+    public static void main(String[] args) {
+        String firstName="Aditya";
+        String fatherName="Anil";
+
+         String surName=firstName.concat(" Gupta");
+         String surname2=fatherName.concat(" Gupta");
+        System.out.println(surName);
+        System.out.println(surname2);
+
+          String s1=new String(" Mohit");
+          String s2=new String("Shohan");
+        System.out.println(s1==s2);
+        System.out.println(s1.equalsIgnoreCase(s2));
+        System.out.println();
+
+    }
+}
