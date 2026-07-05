02b4c0f30f675b2feef9e030849d5a015f8c5ae8
CyberVerve07 <adityamahajan7287@gmail.com>
2026-07-05 21:27:28 +0530

    feat: add static calculation and static block execution examples

---
+++ src/July4/Code5.java
@@ -0,0 +1,21 @@
+package July4;
+
+public class Code5 {
+
+     static void  calcuate(){
+         int num1=10;
+         int num2=20;
+         System.out.println(num1+num2);
+     }
+
+
+
+    public static void main(String[] args){
+
+        // Code5 go=new Code5();
+         //go.calcuate();
+calcuate();
+
+
+    }
+}
---
+++ src/July4/code3.java
@@ -0,0 +1,9 @@
+package July4;
+
+public class code3 {
+    static {
+        System.out.println("Start code without main");
+        System.exit(0);
+        //Only in Java 8
+    }
+}
