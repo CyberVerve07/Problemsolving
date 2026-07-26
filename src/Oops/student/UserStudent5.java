9a6c64f639a06d3a5ff3763a217b4bb9fe0dacac
CyberVerve07 <adityamahajan7287@gmail.com>
2026-07-26 20:38:40 +0530

    feat: implement various OOP exercises covering inheritance, encapsulation, polymorphism, and abstraction across multiple banking and service modules.

---
+++ src/Oops/student/UserStudent5.java
@@ -0,0 +1,10 @@
+package student;
+
+public class UserStudent5 extends Dav {
+    public static void main(String[] args) {
+        Dav Student5=new Dav("Aditya",15,67,"Riya");
+        Student5.setGf("Prachi");
+
+
+    }
+}
---
+++ src/Oops/student/UsesStudent4.java
@@ -0,0 +1,13 @@
+package student;
+
+public class UsesStudent4 {
+    public static void main(String[] args) {
+        Dav Student4 = new Dav("Abhishake ", 30, 44, "Prachi");
+
+        Student4.study();
+        Student4.lunch();
+        Student4.getName();
+
+
+    }
+}
