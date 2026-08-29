9e883749462361784c0bcb6c093ab414b9243421
CyberVerve07 <adityamahajan7287@gmail.com>
2026-08-29 19:41:16 +0530

    feat: add examples for unmodifiable sets and synchronized lists using Collections utility methods

--- src/Collection/Practice/SynchronizedList.java
+++ src/Collection/Practice/SynchronizedList.java
@@ -1,4 +1,31 @@
 package Collection.Practice;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class SynchronizedList {
-}
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+
+         List<String >synchronizedList= Collections.synchronizedList(list);
+
+           synchronizedList.add("Java");
+           synchronizedList.add("spring");
+           synchronizedList.add("Boot");
+
+            //Display the synchronized list
+        System.out.println("Synchronized list:"+synchronizedList);  //Display the Synchronized list;
+
+          synchronized (synchronizedList){// //Iterating over the Synchrnoized list
+
+          for (String item:synchronizedList){
+              System.out.println("list item "+item);
+          }
+
+
+
+
+    }
+}}
--- src/Collection/Practice/UnmodifiableList.java
+++ src/Collection/Practice/UnmodifiableList.java
@@ -1,4 +1,34 @@
 package Collection.Practice;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class UnmodifiableList {
+    public static void main(String[] args) {
+
+        //Create a normal set of electronic items:
+
+            Set<String> electronicSet=new HashSet<>();
+
+              electronicSet.add("I phone15");
+              electronicSet.add("Washing machine");
+              electronicSet.add("Fridge");
+
+
+                  //Make the Set unmodifiable:
+
+        Set<String>unmodifiableElectronic= Collections.unmodifiableSet(electronicSet);
+
+         //print the elements :
+        System.out.println("unmodifiable elements are"+unmodifiableElectronic );
+
+        //Try to umodify the unmodifiable set(will throw exception):
+
+        unmodifiableElectronic.add("Smart watch:");
+
+
+    }
+
+
 }
