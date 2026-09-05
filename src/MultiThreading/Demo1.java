3a50cbcdd7913b6376104bebb7e06f9ae563e877
CyberVerve07 <adityamahajan7287@gmail.com>
2026-09-05 18:56:23 +0530

    feat: add basic multithreading examples by extending Thread and implementing Runnable

--- src/MultiThreading/Demo1.java
+++ src/MultiThreading/Demo1.java
@@ -1,4 +1,23 @@
 package MultiThreading;
 
 public class Demo1 {
+    public static void main(String args[]){
+        //In java thread is a lightweight subprocess.
+         // In java every program have atleast one thread
+        // Java supports multi threading means executing the multiple thread at the same time :
+
+
+            // Print the same number of the current thread :
+
+        System.out.println("Inside main thread:");
+        System.out.println("Thread name :"+Thread.currentThread().getName());
+
+    }
+
 }
+
+  class  MyThread extends Thread{
+    public  void run(){
+        System.out.println("Running the thread : by extending : ");
+    }
+  }
--- src/MultiThreading/Demo2.java
+++ src/MultiThreading/Demo2.java
@@ -1,4 +1,13 @@
 package MultiThreading;
 
-public class Demo2 {
+public class Demo2  implements Runnable {
+    static void main() {
+        
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Running thread by implementing the Runnable: ");
+    }
 }
