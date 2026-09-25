cfd097e3e1bed6694fced377a2ce29886bd4024d
CyberVerve07 <adityamahajan7287@gmail.com>
2026-09-25 19:10:59 +0530

    feat: add examples for TreeMap, mutable Map keys, and thread synchronization

--- src/Collection/Map/Main.java
+++ src/Collection/Map/Main.java
@@ -1,4 +1,35 @@
-package Collection.Map;
+import java.util.*;
 
-public class Example4 {
+class Employee {
+    int id;
+
+    Employee(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Employee &&
+                id == ((Employee) obj).id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee e = new Employee(10);
+
+        Map<Employee, String> map = new HashMap<>();
+
+        map.put(e, "Java");
+
+        e.id = 20;
+
+        System.out.println(map.get(e));
+    }
 }
--- src/Collection/Map/Test.java
+++ src/Collection/Map/Test.java
@@ -1,4 +1,24 @@
 package Collection.Map;
 
-public class Test {
+class Test {
+
+    synchronized void instanceMethod() {
+        System.out.println("Instance");
+    }
+
+    static synchronized void staticMethod() {
+        System.out.println("Static");
+    }
 }
+
+ class Run{
+     public static void main(String[] args) {
+     Test obj1 = new Test();
+     Test obj2 = new Test();
+
+     Thread t1 = new Thread(() -> obj1.instanceMethod());
+     Thread t2 = new Thread(() -> obj2.instanceMethod());
+
+     Thread t3 = new Thread(() -> Test.staticMethod());
+     Thread t4 = new Thread(() -> Test.staticMethod());
+ }}
